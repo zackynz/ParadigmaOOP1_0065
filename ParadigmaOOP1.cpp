bc9a7ffbb6d24ab899ebd541507c2d63d14fef06
zackynz <nurasfanz@gmail.com>
2024-05-20 14:36:18 +0700

    public berarti konstruktor dapat dipanggil dari mana saja, baik dari dalam maupun luar kelas.

--- ParadigmaOOP1.cpp
+++ ParadigmaOOP1.cpp
@@ -5,7 +5,15 @@ class mahasiswa {
 private:
     const int id;
     string nama;
-    float nilai
+    float nilai;
+
+public:
+    mahasiswa(int pId, string pNama, float pNilai) :id(pId),
+    nama(pNama), nilai(pNilai)
+{
+    
+}
+    
 
 
 };
