3f2c283b391fbabeda6359bb316baee3827aee0a
zackynz <nurasfanz@gmail.com>
2024-05-20 15:15:50 +0700

    Baris ini mendeklarasikan kelas bernama anak. Kelas adalah sebuah cetak biru yang menentukan properti (atribut) dan fungsi (metode) dari objek.

--- ParadigmaOOP1.cpp
+++ ParadigmaOOP1.cpp
@@ -11,9 +11,19 @@ public:
     mahasiswa(int pId, string pNama, float pNilai) :id(pId),
     nama(pNama), nilai(pNilai)
 {
-    
+
+}
+
+~mahasiswa () {
+    cout << "Id     = " << id << endl;
+    cout << "Nama   = " << nama << endl;
+    cout << "Nilai  = " << nilai << endl;
 }
     
-
-
 };
+
+int main() {
+    mahasiswa mhs(12, "Asroni", 90.5);
+
+    return 0;
+}
