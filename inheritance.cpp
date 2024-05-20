d1dcf7ed2597e3b3e3763584a38dca12a10bcc2d
zackynz <nurasfanz@gmail.com>
2024-05-20 19:56:59 +0700

    mendeklarasikan interger untuk menampilkan nama siswa dan jumlahnya

--- inheritance.cpp
+++ inheritance.cpp
@@ -32,3 +32,10 @@ public:
         return "Hallo, nama saya" + nama + "dari sekolah" + sekolah + "\n\n";
     }
 };
+int main() {
+    pelajar siswa1("andi laksono", "SMAAN 1 Bantul");
+    cout << siswa1.perkenalan();
+    cout << "Hasil = " << siswa1.jumlah(10, 90) << endl;
+
+    return 0;
+}
