ec77399fa44ec77d624150502b06e43897ab24b7
zackynz <nurasfanz@gmail.com>
2024-05-20 19:37:46 +0700

    mendeklarasikan void pasien pada cektakdokter untuk mencetak daftar dokter yang menangani pasien.

--- Asosiasi.cpp
+++ Asosiasi.cpp
@@ -36,4 +36,10 @@ public:
 void pasien::tambahDokter(dokter* pDokter) {
     daftar_dokter.push_back(pDokter);
 }
-
+void pasien::cetakDokter() {
+    cout << "Daftar Dokter yang menangani pasien \"" << this->nama << "\":\n";
+    for (auto& a : daftar_dokter) {
+        cout << a->nama << "\n";
+    }
+    cout << endl;
+}
