1f78dd5d14dd694e72439de929113b31b84a4d84
zackynz <nurasfanz@gmail.com>
2024-05-20 19:43:32 +0700

    mendeklarasikan void pada dokter untuk mencetak daftar pasien dari dokter yang bertugas

--- Asosiasi.cpp
+++ Asosiasi.cpp
@@ -47,5 +47,9 @@ void dokter ::tambahPasien(pasien* pPasien) {
     daftar_pasien.push_back(pPasien);
     pPasien->tambahDokter(this);
 }
-
+void dokter::cetakPasien() {
+    cout << "Daftar Pasien dari dokter \"" << this->nama << "\":\n";
+    for (auto& a : daftar_pasien) {
+        cout << a->nama << "\n";
+    }
 }
