bff697c0318d9eb46b55a934e6409a10842f4a6e
zackynz <nurasfanz@gmail.com>
2024-05-20 19:55:31 +0700

    membuat class untuk orang

---
+++ inheritance.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class orang {
+public:
+    string nama;
+    orang(string pNama) :
+        nama(pNama) {
+        cout << "orang dibuat\n" << endl;
+    }
+    ~orang() {
+        cout << "orang dihapus\n" << endl;
+    }
+
+    int jumlah(int a, int b) {
+        return a + b;
+    }
+};
+
